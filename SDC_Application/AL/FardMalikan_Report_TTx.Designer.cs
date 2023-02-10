8302264b9616faad3c58eff697e25495ad291334
shehzadafzal <shehzadafzal@gmail.com>
2023-02-10 14:16:27 +0500

    10_feb_2023_2_16pm

---
+++ SDC_Application/AL/FardMalikan_Report_TTx.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SDC_Application.AL
+{
+    partial class FardMalikan_Report_TTx
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(78, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(746, 376);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(996, 262);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(432, 154);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(798, 561);
+            this.reportViewer1.TabIndex = 1;
+            // 
+            // FardMalikan_Report_TTx
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1436, 796);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FardMalikan_Report_TTx";
+            this.Text = "FardMalikan_Report_TTx";
+            this.Load += new System.EventHandler(this.FardMalikan_Report_TTx_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ SDC_Application/AL/FardMalikan_Report_TTx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace SDC_Application.AL
+{
+    public partial class FardMalikan_Report_TTx : Form
+    {
+        public FardMalikan_Report_TTx()
+        {
+            InitializeComponent();
+        }
+
+
+        public long TokenId = -1;
+        
+
+        private void FardMalikan_Report_TTx_Load(object sender, EventArgs e)
+            {
+                TokenId = 43202301100001;
+
+                string dsvr =SDC_Application.Classess.Crypto.Decrypt(System.Configuration.ConfigurationSettings.AppSettings["server"]);
+                string db =SDC_Application.Classess.Crypto.Decrypt(System.Configuration.ConfigurationSettings.AppSettings["db"]);            
+                string password = SDC_Application.Classess.Crypto.Decrypt(System.Configuration.ConfigurationSettings.AppSettings["allow"]);
+                string connectionString = "Data Source=" + dsvr + ";Initial Catalog=" + db + ";user id=dlis; Password=" + password + ";MultipleActiveResultSets=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    int employeeID = 0;
+                    try
+                    {
+                        connection.Open();
+                        SqlCommand command = new SqlCommand("clrmis_Sp_FardMalakan_TTx", connection);
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.Add(new SqlParameter("@TokenId", TokenId));
+                        command.CommandTimeout = 5;
+
+                        SqlDataAdapter adapter = new SqlDataAdapter(command);
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+
+                        /*
+                                            this.cmbKhata.DataSource = ds.Tables[0];
+                                            this.cmbKhata.DisplayMember = "KhataNo";
+                                            this.cmbKhata.ValueMember = "RegisterHqDKhataId";
+                        */
+                        //command.ExecuteNonQuery();
+                        this.dataGridView1.DataSource = ds.Tables[0];
+
+                        ReportDocument cryRpt = new ReportDocument();
+                        //cryRpt.Load("SDC_Application.AL.FardMalakan_TTx.rpt");
+                        //cryRpt.Load("SDC_Application.AL.FardMalakan_TTx.rpt");
+                        cryRpt.Load(@"AL\FardMalakan_TTx.rpt");
+                        crystalReportViewer1.ReportSource = cryRpt;
+                        crystalReportViewer1.Refresh();
+                        crystalReportViewer1.Show();
+                        crystalReportViewer1.ShowLastPage();
+                        
+                     
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+
+                this.reportViewer1.RefreshReport();
+        }
+    }
+}
